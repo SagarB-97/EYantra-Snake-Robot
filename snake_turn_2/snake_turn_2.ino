4c6a9e2ce23e8fbc4d8c1d487774978f54d1b744
Prajwal B Bharadwaj <34282102+PrajwalBBharadwaj@users.noreply.github.com>
2018-02-17 11:22:13 +0530

    Update snake_turn_2.ino

--- snake_turn_2/snake_turn_2.ino
+++ snake_turn_2/snake_turn_2.ino
@@ -7,7 +7,7 @@ Servo servo2,servo3,servo4,servo5,servo6,servo7,servo8;
 
 int pos2_init = 70;
 int pos3_init = 110;
-int pos4_init = 115;
+int pos4_init = 77;
 int pos5_init = 115;
 int pos6_init = 120;
 int pos7_init = 82;
