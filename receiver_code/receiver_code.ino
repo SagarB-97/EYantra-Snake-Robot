209e20cd05e1145e9fc9d40c6000158a055caea9
Sagar Bharadwaj <sagar.bharadwaj@yahoo.com>
2018-02-20 23:05:12 +0530

    Updated Receiver code

--- receiver_code/receiver_code.ino
+++ receiver_code/receiver_code.ino
@@ -15,9 +15,9 @@ const byte address[6] = "00001";
 int pos2_init = 70;
 int pos3_init = 110;
 int pos4_init = 77;
-int pos5_init = 121;
+int pos5_init = 95;
 int pos6_init = 120;
-int pos7_init = 78;
+int pos7_init = 62;
 int pos8_init = 80;
 
 
@@ -79,32 +79,56 @@ void forward(){
   delay(100);
 }
 void backward(){
+  if(state!='b')
+    forwardReset();
+
+  int pos8_val = pos8_init + floor(forwardParams.amplitude_h*cos(forwardParams.frequency*forwardParams.counter*PI/180));
+  servo8.write(pos8_val);
+
+  servo3.write(pos3_init);
+
+  int pos6_val = pos6_init - floor(forwardParams.amplitude_h*cos(forwardParams.frequency*forwardParams.counter*PI/180 - 1*forwardParams.lag)); 
+  servo6.write(pos6_val);
+
+  servo5.write(pos5_init);
+
+  int pos4_val = pos4_init + floor(forwardParams.amplitude_h*cos(forwardParams.frequency*forwardParams.counter*PI/180 - 2*forwardParams.lag));
+  servo4.write(pos4_val);
+
+  servo7.write(pos7_init);
+
+  int pos2_val = pos2_init - floor(forwardParams.amplitude_h*cos(forwardParams.frequency*forwardParams.counter*PI/180 - 3*forwardParams.lag));
+  servo2.write(pos2_val);
+
+  forwardParams.counter = (forwardParams.counter+10)%360;
+  delay(100);
 }
 
 void turnReset(){
   turnParams.counter = 0;
   turnParams.frequency1 = 2;
   turnParams.frequency2 = 2;
-  turnParams.lag1 = 240*PI/180;
-  turnParams.lag2 = 180*PI/180;
   turnParams.a = 130;
   turnParams.b = 2*PI;
+  turnParams.c = 0;
   turnParams.n = 3;
+  turnParams.lag1 = 240*PI/180;
+  turnParams.lag2 = 180*PI/180;
   turnParams.beta = turnParams.b/turnParams.n;
   turnParams.alpha = abs(turnParams.a*sin(turnParams.beta/2));
   turnParams.gamma = -turnParams.c/turnParams.n;
-  turnParams.amplitude_h = 10;
+  turnParams.amplitude_h = 20;
 }
 void leftReset(){
   turnReset();
-  turnParams.c = 10;
 }
 void rightReset(){
   turnReset();
-  turnParams.c = -10;
 }
 
-void turn(){
+void right(){
+  if(state!='r') rightReset();
+    
   int pos2_val = pos2_init + floor(-(turnParams.amplitude_h*sin(turnParams.frequency2*turnParams.counter*PI/180 + turnParams.b + 0*turnParams.lag2)));
   servo2.write(pos2_val);
   
@@ -130,13 +154,33 @@ void turn(){
 
   delay(10);
 }
-void right(){
-  if(state!='r') rightReset();
-  turn();
-}
 void left(){
   if(state!='l') leftReset();
-  turn();
+  
+  int pos2_val = pos2_init + floor(-(turnParams.amplitude_h*sin(turnParams.frequency2*turnParams.counter*PI/180 + turnParams.b + 0*turnParams.lag2)));
+  servo2.write(pos2_val);
+  
+  int pos3_val = pos3_init + floor(turnParams.alpha*sin(turnParams.frequency1*turnParams.counter*PI/180 + 0*turnParams.beta)+turnParams.gamma);
+  servo3.write(pos3_val); 
+
+  int pos4_val = pos4_init + floor((turnParams.amplitude_h*sin(turnParams.frequency2*turnParams.counter*PI/180 + turnParams.b + 1*turnParams.lag2)));
+  servo4.write(pos4_val);
+
+   int pos5_val = pos5_init + floor((turnParams.alpha*sin(turnParams.frequency1*turnParams.counter*PI/180 + 1*turnParams.beta))+turnParams.gamma);
+  servo5.write(pos5_val);
+
+  int pos6_val = pos6_init + floor(-(turnParams.amplitude_h*sin(turnParams.frequency2*turnParams.counter*PI/180 + turnParams.b + 2*turnParams.lag2)));
+  servo6.write(pos6_val);
+
+   int pos7_val = pos7_init + floor(-turnParams.alpha*sin(turnParams.frequency1*turnParams.counter*PI/180 + 2*turnParams.beta)+turnParams.gamma);
+  servo7.write(pos7_val);
+
+  int pos8_val = pos8_init + floor(turnParams.amplitude_h*sin(turnParams.frequency2*turnParams.counter*PI/180 + turnParams.b + 3*turnParams.lag2));
+  servo8.write(pos8_val);
+
+  turnParams.counter = (turnParams.counter+1)%360;
+
+  delay(10);
 }
 
 void equilibrium(){
@@ -193,11 +237,12 @@ void loop() {
     }    
     else if(strcmp(inp,"gt")==0){
       equilibrium();
-      state = 'g';
+      state = 's';
     }
   }
 
   else{
     equilibrium();
+    state='s';
   }
 }
