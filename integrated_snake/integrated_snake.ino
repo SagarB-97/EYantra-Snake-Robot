8efddfc3a29dd8e7581ad122a692593c92f72aa8
Nagarakshith1 <34233808+Nagarakshith1@users.noreply.github.com>
2018-02-21 17:44:06 +0530

    Add files via upload

---
+++ integrated_snake/integrated_snake.ino
@@ -0,0 +1,346 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Servo.h>
+#include <math.h>
+#include <string.h>
+
+RF24 radio(9, 10); // CE, CSN
+const byte address[6] = "00001";
+
+Servo servo2,servo3,servo4,servo5,servo6,servo7,servo8;
+
+int pos2_init = 70;
+int pos3_init = 110;
+int pos4_init = 77;
+int pos5_init = 95;
+int pos6_init = 120;
+int pos7_init = 62;
+int pos8_init = 80;
+
+int flagc=0;
+int flagcb=0;
+int flaglt=0;
+int flagrt=0;
+int flagsd=0;
+
+void setup(){
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0,address);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.startListening();
+
+  servo2.attach(2);
+  servo3.attach(3);
+  servo4.attach(4);
+  servo5.attach(5);
+  servo6.attach(6);
+  servo7.attach(7);
+  servo8.attach(8);
+
+  servo2.write(pos2_init);
+  servo3.write(pos3_init);
+  servo4.write(pos4_init);
+  servo5.write(pos5_init);
+  servo6.write(pos6_init);
+  servo7.write(pos7_init);
+  servo8.write(pos8_init);
+}
+void equilibrium(){
+  servo2.write(pos2_init);
+  servo3.write(pos3_init);
+  servo4.write(pos4_init);
+  servo5.write(pos5_init);
+  servo6.write(pos6_init);
+  servo7.write(pos7_init);
+  servo8.write(pos8_init);
+}
+void caterpillar(){
+  static  int counter,frequency,amplitude,amplitude_h;
+  static float lag;
+  if(flagc==0){
+   counter = 0;
+   frequency = 4;
+   lag = PI/2;
+   amplitude = 0;
+   amplitude_h = 40;
+   flagc=1;
+  }
+  int pos2_val = pos2_init + floor(amplitude_h*cos(frequency*counter*PI/180));
+  servo2.write(pos2_val); 
+  servo3.write(pos3_init);
+
+  int pos4_val = pos4_init - floor(amplitude_h*cos(frequency*counter*PI/180 - 1*lag)); 
+  servo4.write(pos4_val);
+  servo5.write(pos5_init);
+
+  int pos6_val = pos6_init + floor(amplitude_h*cos(frequency*counter*PI/180 - 2*lag));
+  servo6.write(pos6_val); 
+  servo7.write(pos7_init);
+
+  int pos8_val = pos8_init - floor(amplitude_h*cos(frequency*counter*PI/180 - 3*lag));
+  servo8.write(pos8_val); 
+  counter = (counter+10)%360;
+  delay(100);
+}
+void caterpillar_b(){
+  static  int counter,frequency,amplitude,amplitude_h;
+  static float lag;
+  if(flagcb==0){
+   counter = 0;
+   frequency = 4;
+   lag = PI/2;
+   amplitude = 0;
+   amplitude_h = 40;
+   flagcb=1;
+  }
+  int pos8_val = pos8_init + floor(amplitude_h*cos(frequency*counter*PI/180));
+  servo8.write(pos8_val); 
+  servo3.write(pos3_init);
+
+  int pos6_val = pos6_init - floor(amplitude_h*cos(frequency*counter*PI/180 - 1*lag)); 
+  servo6.write(pos6_val);
+  servo5.write(pos5_init);
+
+  int pos4_val = pos4_init + floor(amplitude_h*cos(frequency*counter*PI/180 - 2*lag));
+  servo4.write(pos4_val); 
+  servo7.write(pos7_init);
+
+  int pos2_val = pos2_init - floor(amplitude_h*cos(frequency*counter*PI/180 - 3*lag));
+  servo2.write(pos2_val); 
+  counter = (counter+10)%360;
+  delay(100);
+}
+void lturning(){
+  static int counter;
+  static float frequency1,frequency2,lag1,lag2,a,b,c,n,amplitude_h,beta,alpha,gamma;
+  if(flaglt==0){
+    counter = 0;
+    frequency1 = 2;
+    frequency2 = 2;
+    lag1 = 240*PI/180;
+    lag2 = 180*PI/180;
+    a=80;
+    b =2*PI;
+    c=0;
+    n=3;
+    beta = b/n;
+    alpha = abs(a*sin(beta/2));
+    gamma = -c/n;
+    amplitude_h = 30;
+    flaglt=1;
+  }
+  int pos2_val = pos2_init + floor(-(amplitude_h*sin(frequency2*counter*PI/180 + b + 0*lag2)));
+  servo2.write(pos2_val); 
+
+  int pos3_val = pos3_init + floor(alpha*sin(frequency1*counter*PI/180 + 0*beta)+gamma);
+  servo3.write(pos3_val); 
+
+  int pos4_val = pos4_init + floor((amplitude_h*sin(frequency2*counter*PI/180 + b + 1*lag2)));
+  servo4.write(pos4_val); 
+
+  int pos5_val = pos5_init + floor((alpha*sin(frequency1*counter*PI/180 + 1*beta))+gamma);
+  servo5.write(pos5_val);
+  
+  int pos6_val = pos6_init + floor(-(amplitude_h*sin(frequency2*counter*PI/180 + b + 2*lag2)));
+  servo6.write(pos6_val); 
+  
+  int pos7_val = pos7_init + floor(-alpha*sin(frequency1*counter*PI/180 + 2*beta)+gamma);
+  servo7.write(pos7_val);
+  
+  int pos8_val = pos8_init + floor(amplitude_h*sin(frequency2*counter*PI/180 + b + 3*lag2));
+  servo8.write(pos8_val);
+  
+  counter = (counter+1)%360;
+  delay(10);
+}
+void rturning(){
+  static int counter;
+  static float frequency1,frequency2,lag1,lag2,a,b,c,n,amplitude_h,beta,alpha,gamma;
+  if(flagrt==0){
+    counter = 0;
+    frequency1 = 2;
+    frequency2 = 2;
+    lag1 = 240*PI/180;
+    lag2 = 180*PI/180;
+    a=80;
+    b =2*PI;
+    c=0;
+    n=3;
+    beta = b/n;
+    alpha = abs(a*sin(beta/2));
+    gamma = -c/n;
+    amplitude_h = 30;
+    flagrt=1;
+  }
+  int pos2_val = pos2_init + floor(-(amplitude_h*sin(frequency2*counter*PI/180 + b + 0*lag2)));
+  servo2.write(pos2_val); 
+
+  int pos3_val = pos3_init + floor(-alpha*sin(frequency1*counter*PI/180 + 0*beta)+gamma);
+  servo3.write(pos3_val); 
+
+  int pos4_val = pos4_init + floor((amplitude_h*sin(frequency2*counter*PI/180 + b + 1*lag2)));
+  servo4.write(pos4_val); 
+
+  int pos5_val = pos5_init + floor(-(alpha*sin(frequency1*counter*PI/180 + 1*beta))+gamma);
+  servo5.write(pos5_val);
+  
+  int pos6_val = pos6_init + floor(-(amplitude_h*sin(frequency2*counter*PI/180 + b + 2*lag2)));
+  servo6.write(pos6_val); 
+  
+  int pos7_val = pos7_init + floor(alpha*sin(frequency1*counter*PI/180 + 2*beta)+gamma);
+  servo7.write(pos7_val);
+  
+  int pos8_val = pos8_init + floor(amplitude_h*sin(frequency2*counter*PI/180 + b + 3*lag2));
+  servo8.write(pos8_val);
+  
+  counter = (counter+1)%360;
+  delay(10);
+}
+
+void sidewinding(){
+  static int counter;
+  static float frequency1,frequency2,lag1,lag2,b,amplitude_h,amplitude;
+  if(flagsd==0){
+    counter = 0;
+    frequency1 = 2;
+    frequency2 = 2;
+    lag1 = 240*PI/180;
+    lag2 = 180*PI/180;
+    b = 120*PI/180;
+    amplitude_h = 10;
+    amplitude = 80;
+    flagsd=1;
+ }
+  int pos2_val = pos2_init + floor(-(amplitude_h*sin(frequency2*counter*PI/180 + b + 0*lag2)));
+  servo2.write(pos2_val); 
+  Serial.println(pos2_val);
+  
+  
+  int pos3_val = pos3_init + floor(amplitude*sin(frequency1*counter*PI/180 + 0*lag1));
+  servo3.write(pos3_val); 
+  
+
+  int pos4_val = pos4_init + floor(amplitude_h*sin(frequency2*counter*PI/180 + b + 1*lag2));
+  servo4.write(pos4_val); 
+  
+
+  int pos5_val = pos5_init + floor(-(amplitude*sin(frequency1*counter*PI/180 + 1*lag1)));
+  servo5.write(pos5_val);
+  
+  int pos6_val = pos6_init + floor(-(amplitude_h*sin(frequency2*counter*PI/180 + b + 2*lag2)));
+  servo6.write(pos6_val); 
+  
+  int pos7_val = pos7_init + floor(amplitude*sin(frequency1*counter*PI/180 + 2*lag2));
+  servo7.write(pos7_val);
+  
+  int pos8_val = pos8_init + floor(amplitude_h*sin(frequency2*counter*PI/180 + b + 3*lag2));
+  servo8.write(pos8_val);
+  
+  counter = (counter+1)%360;
+
+  delay(20);
+  //Serial.println("xxx");
+}
+
+void loop() {
+  //char text[32]="";
+  long int i, count=0;
+  char a[2]={'g','t'};
+  char b[2]={'n','m'};
+  flagc=0;
+  flagcb=0;
+  flagrt=0;
+  flaglt=0;
+  
+    if (radio.available()) {
+    char text[5] = "";
+    radio.read(text, sizeof(text));
+      //Serial.println(text);
+    if(text[0]==a[0] && text[1]==a[1]){
+      //Serial.println(text);
+      count+=1;
+      for(i=0;i<50000;i++){
+          if(radio.available()){
+            radio.read(text,sizeof(text));
+            if(text[0]==a[0] && text[1]==a[1])
+              count+=1;
+          }
+      }
+      Serial.println(count);
+      //Serial.println("over");
+  }
+  }
+  
+  if(count==1){
+    char text_1[5]="";
+    while(1){
+      if(radio.available()){
+        radio.read(text_1,sizeof(text_1));  
+        if(text_1[0]=='f')  
+          {caterpillar();}
+           //Serial.println('c');}
+        else if(text_1[0]=='b')
+          {caterpillar_b();}
+        else if(text_1[0]=='l')
+          {lturning();}
+        else if(text_1[0]=='r')
+          {rturning();}
+        else if(text_1[0]==b[0] && text_1[1]==b[1])
+           break;
+       }
+      else{
+        equilibrium();
+        flagc=0;
+        flagcb=0;
+        flagrt=0;
+        flaglt=0; 
+      }
+    
+    }
+  }
+//  if(count==2){
+//    char text_2[5]="";
+//    while(1){
+//      if(radio.available()){
+//        radio.read(text_2,sizeof(text_2));
+//        if(text_2[0]=='l')  
+//        { turning();
+//          Serial.println('t');}
+//        else if(text_2[0]==b[0] && text_2[1]==b[1])
+//          break; 
+//     }
+//     else{
+//        equilibrium();
+//        flagt=0; 
+//     }
+//   }
+// }
+  if(count==2){
+    char text_3[5]="";
+    while(1){
+      if(radio.available()){
+        radio.read(text_3,sizeof(text_3));
+        if(text_3[0]=='l')  
+        { sidewinding();
+          Serial.println('sd');}
+        else if(text_3[0]==b[0] && text_3[1]==b[1])
+          break; 
+     }
+     else{
+        equilibrium();
+        flagsd=0; 
+     }
+   }
+  }
+    count=0;        
+  }     
+      
+    
+  
+  
+  
+  
+  
+
---
+++ rodent_deetection1/rodent_deetection1.ino
@@ -0,0 +1,136 @@
+/*     Arduino Color Sensing Tutorial
+ *      
+ *  by Dejan Nedelkovski, www.HowToMechatronics.com
+ *  
+ */
+// pin 9 for red light and 10 for blue light
+#include <Servo.h>
+Servo myservo;  // create servo object to control a servo
+
+#define S0 4
+#define S1 5
+#define S2 6
+#define S3 7
+#define sensorOut 8
+
+int pos1= 20;
+int pos2= 180;
+int flag=0;
+void setup() {
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(sensorOut, INPUT);
+  digitalWrite(9,HIGH);
+  digitalWrite(10,HIGH);
+  myservo.attach(11);
+  
+  // Setting frequency-scaling to 20%
+  digitalWrite(S0,HIGH);
+  digitalWrite(S1,LOW);
+  
+  Serial.begin(9600);
+}
+char color_detector(){
+  int frequency_r,frequency_b,frequency,r_th,b_th;
+  r_th=100;
+  b_th=100;
+  char out;
+    // Setting red filtered photodiodes to be read
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,LOW);
+  // Reading the output frequency
+  frequency_r = pulseIn(sensorOut, LOW);
+  // Printing the value on the serial monitor
+  //Serial.print("R= ");//printing name
+  //Serial.print(frequency_r);//printing RED color frequency
+  //Serial.print("  ");
+  delay(10);
+
+  // Setting Green filtered photodiodes to be read
+  digitalWrite(S2,HIGH);
+  digitalWrite(S3,HIGH);
+  // Reading the output frequency
+  frequency = pulseIn(sensorOut, LOW);
+  // Printing the value on the serial monitor
+  //Serial.print("G= ");//printing name
+  //Serial.print(frequency);//printing RED color frequency
+  //Serial.print("  ");
+  delay(100);
+
+  // Setting Blue filtered photodiodes to be read
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,HIGH);
+  // Reading the output frequency
+  frequency_b = pulseIn(sensorOut, LOW);
+  // Printing the value on the serial monitor
+  //Serial.print("B= ");//printing name
+  //Serial.print(frequency_b);//printing RED color frequency
+  //Serial.println("  ");
+  delay(100);
+  if(frequency>100 && frequency_b>70 && frequency_r<90 )
+    out='r';
+  else if(frequency>60 && frequency_b<60 && frequency<100)
+    out='b';
+  return out;
+}
+void led_on(char a){
+  //char z;
+  //z=a;
+  if (a=='r'){
+    digitalWrite(9,LOW);
+    digitalWrite(10,HIGH);
+   
+   }
+  else if (a=='b'){
+    digitalWrite(10,LOW);
+    digitalWrite(9,HIGH);
+    
+}}
+void servo(){
+    myservo.write(pos2);// tell servo to go to position in variable 'pos'
+    if(flag==0)
+    delay(1000);
+}
+void servo_clear(){
+    myservo.write(pos1);              // tell servo to go to position in variable 'pos'
+    delay(1); 
+}
+void led_off(){
+    digitalWrite(9,HIGH);
+    digitalWrite(10,HIGH);
+  }
+void buzzer(){
+  if(flag==0){
+   digitalWrite(12,HIGH);
+   //Serial.println("bzzzzz");
+   delay(1000);
+   digitalWrite(12,LOW);
+   delay(500);
+   digitalWrite(12,HIGH);
+   delay(1000);
+   digitalWrite(12,LOW);
+   }
+}
+void loop(){
+  char o;
+  o=color_detector();
+  servo_clear();
+  if((o=='r' || o=='b')){
+  led_on(o);
+  //buzzer();
+  servo();
+  buzzer();
+  flag=1;
+  Serial.println(o);
+  }
+  else //if(flag==1)
+  { //Serial.println('a');
+    flag=0;
+   servo_clear();
+  led_off();}
+}
